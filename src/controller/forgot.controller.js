0f0141e0cafd6472771f7229d4ec8993979d71f0
prospyb <ekuajiprosper@gmail.com>
2023-05-29 19:43:19 +0100

    worked on forgot password

--- src/controller/forgot.controller.js
+++ src/controller/forgot.controller.js
@@ -1,14 +1,18 @@
+import express from "express";
 import User from "../model/user.model.js";
 import { codeReset } from "../utils/codeGeneration.js";
-
+import dotenv from "dotenv";
 import {
   passwordEmailValidator,
   verifyCode,
   verifyPasswordField,
 } from "../validators/user.validator.js";
-import { verified } from "../utils/jwt.utils.js";
 import { userPassword } from "../utils/passwordHashing.js";
 import { sentMail } from "../Email/emailSending.js";
+dotenv.config();
+
+const router = express.Router();
+
 const arrMail = [];
 
 export const emailForPassword = async (req, res) => {
@@ -17,22 +21,22 @@ export const emailForPassword = async (req, res) => {
     const { email } = req.body;
     const found = await User.findOne({ email: email }, { password: 0 });
     if (found) {
-    arrMail.push(found._id);
-      // console.log(req.headers.cookies)
+      req.app.locals.userMail = found.email;
+      arrMail.push(found.email);
       const userCode = codeReset();
-      found.resetCode = `${userCode}`;
+      found.resetCode = userCode;
       found.save();
-      await sentMail(found.email, `${userCode}`);
+      await sentMail(found.email, userCode);
       process.env.FOUNDUSERID = found._id;
       res.status(200).json({
         success: true,
         message:
-          "a reset code has been sent to your email, please ensure to check both your inbox and spam",
+          "A reset code has been sent to your email. Please ensure to check both your inbox and spam.",
       });
     } else {
       res.status(404).json({
         success: false,
-        message: " user not found",
+        message: "User not found.",
         data: {},
       });
     }
@@ -42,26 +46,21 @@ export const emailForPassword = async (req, res) => {
 };
 
 export const codeResetVerification = async (req, res) => {
-  // const user = verified(req.user);
   const { resetCode } = req.body;
   const { error } = verifyCode.validate(req.body);
   if (!error) {
-    const userId = arrMail[0];
-    const foundUser = await User.findOne({
-      email: `${userId}`,
-      resetCode:  resetCode,
-    });
+    const userEmail = req.app.locals.userMail;
+    const foundUser = await User.findOne({ email: userEmail, resetCode: resetCode });
     if (foundUser) {
-      //redirect to forgot password page
       res.status(200).json({
         success: true,
-        message: "successfully changed",
+        message: "Successfully changed.",
         data: {},
       });
     } else {
       res.status(400).json({
         success: false,
-        message: "you entered an invalid code",
+        message: "You entered an invalid code.",
         data: {},
       });
     }
@@ -70,21 +69,30 @@ export const codeResetVerification = async (req, res) => {
   }
 };
 
+
 export const passwordReset = async (req, res) => {
   const { password } = req.body;
-  const { error } = verifyPasswordField.validate(req.bod);
+  const { error } = verifyPasswordField.validate(req.body);
   if (!error) {
-     const userId = arrMail[0];
-    const foundUser = await User.findOne({ email: `${userId}` });
-    const newPassword = userPassword(password);
-    foundUser.password = `${newPassword}`;
-    foundUser.save();
-    res.status(200).json({
-      success: true,
-      message: "password successfully updated",
-      data: {},
-    });
-     arrMail[0]='';
+    const userEmail = req.app.locals.userMail;
+    const foundUser = await User.findOne({ email: userEmail });
+    if (foundUser) {
+      const newPassword = await userPassword(password);
+      foundUser.password = newPassword;
+      foundUser.save();
+      res.status(200).json({
+        success: true,
+        message: "Password successfully updated.",
+        data: {},
+      });
+      arrMail[0] = "";
+    } else {
+      res.status(404).json({
+        success: false,
+        message: "User not found.",
+        data: {},
+      });
+    }
   } else {
     res.status(422).json(error);
   }
