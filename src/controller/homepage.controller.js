a0d59f249d8a2173c2a4631434a2f260c4ed905f
prospyb <ekuajiprosper@gmail.com>
2023-06-02 08:01:35 +0100

    worked on homepage route

---
+++ src/controller/homepage.controller.js
@@ -0,0 +1,12 @@
+// Get Homepage Content
+export async function getHomepageContent(req, res) {
+
+  const user = req.user;
+
+
+  res.status(200).json({
+    message: "Homepage content retrieved successfully",
+    status: "Success"
+  });
+}
+
---
+++ src/router/homepage.route.js
@@ -0,0 +1,12 @@
+import express from "express";
+import { userAuthMiddleWare } from "../middlewares/auth.middleware.js";
+import { tryCatch } from "../utils/tryCatchHandler.js";
+import { getHomepageContent } from "../controller/homepage.controller.js";
+
+const router = express.Router();
+
+// Homepage Content Route
+router.get("/", userAuthMiddleWare, tryCatch(getHomepageContent));
+
+
+export { router };
