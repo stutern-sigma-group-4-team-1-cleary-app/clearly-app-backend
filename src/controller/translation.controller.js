1643ca9ba414626cd10e24283d75dc9d249d8a89
DevTinaTi <tinadesk8@gmail.com>
2023-06-12 18:38:49 +0100

    translation fixed

--- src/controller/translation.controller.js
+++ src/controller/translation.controller.js
@@ -1,4 +1,4 @@
-import { translation } from "../validators/user.validator";
+import { translation } from "../validators/user.validator.js";
 import axios from "axios";
 import dotenv from "dotenv";
 dotenv.config();
@@ -8,63 +8,74 @@ export const translateFromTextToText = async (req, res) => {
   if (!error) {
     const { option, value, from, to } = req.body;
     if (option == "text") {
-      const options = {
-        method: "POST",
-        url: "https://translate-plus.p.rapidapi.com/translate",
-        headers: {
-          "content-type": "application/json",
-          "X-RapidAPI-Key": `${process.env.SPEECH_KEY}`,
-          "X-RapidAPI-Host": "translate-plus.p.rapidapi.com",
-        },
-        data: {
-          text: `${value}`,
-          source: `${from}`,
-          target: `${to}`,
-        },
-      };
-
-      try {
-        const response = await axios.request(options);
-        const item ={...response};
-        res.status(200).json({
-          success: true,
-          message: "Translation was successful",
-          data:item
-        });
-      } catch (error) {
-        res.status(400).json({
-          success: false,
-          message: "Translation failed",
-          data: {},
-        });
-      }
-    } else if (option == "sound") {
       const encodedParams = new URLSearchParams();
+      encodedParams.set("source_language", `${from}`);
+      encodedParams.set("target_language", `${to}`);
       encodedParams.set("text", `${value}`);
-      encodedParams.set("language_code", `${to}`);
-      encodedParams.set("gender", "male");
 
       const options = {
         method: "POST",
-        url: "https://text-to-speech7.p.rapidapi.com/voice",
+        url: "https://text-translator2.p.rapidapi.com/translate",
         headers: {
           "content-type": "application/x-www-form-urlencoded",
           "X-RapidAPI-Key": `${process.env.SPEECH_KEY}`,
-          "X-RapidAPI-Host": "text-to-speech7.p.rapidapi.com",
+          "X-RapidAPI-Host": "text-translator2.p.rapidapi.com",
         },
         data: encodedParams,
       };
 
       try {
         const response = await axios.request(options);
-        const item = { ...response };
-        res.status(200).json({
+        const newResponse = response.data;
+        const item = { ...newResponse };
+        return res.status(200).json({
           success: true,
           message: "Translation successful",
           data: item,
         });
       } catch (error) {
-        res.status(400).json({
+        return res.status(400).json({
+          success: false,
+          message: "Translation failed",
+          data: {},
+        });
+      }
+    } else if (option == "sound") {
+      const options = {
+        method: "POST",
+        url: "https://joj-text-to-speech.p.rapidapi.com/",
+        headers: {
+          "content-type": "application/json",
+          "X-RapidAPI-Key":
+            "8cadc44954msh0c2f19c00698a0bp16c2a0jsn4b7b57b4733f",
+          "X-RapidAPI-Host": "joj-text-to-speech.p.rapidapi.com",
+        },
+        data: {
+          input: {
+            text: `${value}`,
+          },
+          voice: {
+            languageCode: "en-US",
+            name: "en-US-News-L",
+            ssmlGender: "FEMALE",
+          },
+          audioConfig: {
+            audioEncoding: "MP3",
+          },
+        },
+      };
+
+      try {
+        const response = await axios.request(options);
+        const newResponse = response.data;
+        const item = { ...newResponse };
+        return res.status(200).json({
+          success: true,
+          message: "Translation was successful",
+          data: item,
+        });
+      } catch (error) {
+        return res.status(400).json({
           success: false,
           message: "Translation failed",
           data: {},
@@ -100,17 +111,22 @@ export const translateFromSpeech = async (req, res) => {
     try {
       const response = await axios.request(options);
       const item = { ...response };
-      res.status(200).json({
+      return res.status(200).json({
         success: true,
         message: "Speech to text was successful",
         data: item,
       });
     } catch (error) {
-      res.status(400).json({
+      return res.status(400).json({
         success: false,
         message: "Translation failed",
         data: {},
       });
     }
   }
+  return res.status(422).json({
+    success: false,
+    message: "failed validation",
+    data: error,
+  });
 };
