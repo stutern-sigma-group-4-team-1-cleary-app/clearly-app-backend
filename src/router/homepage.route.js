9d6e2589cba1cbd61619764f5651958e77787ef5
prospyb <ekuajiprosper@gmail.com>
2023-07-02 13:28:04 +0100

    token removed

--- src/router/homepage.route.js
+++ src/router/homepage.route.js
@@ -1,12 +1,12 @@
 import express from "express";
-import { userAuthMiddleWare } from "../middlewares/auth.middleware.js";
+// import { userAuthMiddleWare } from "../middlewares/auth.middleware.js";
 import { tryCatch } from "../utils/tryCatchHandler.js";
 import { getHomepageContent } from "../controller/homepage.controller.js";
 
 const router = express.Router();
 
 // Homepage Content Route
-router.get("/", userAuthMiddleWare, tryCatch(getHomepageContent));
+router.get("/", tryCatch(getHomepageContent));
 
 
 export { router };
